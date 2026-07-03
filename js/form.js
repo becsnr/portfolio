bf6959c171674264b37a63bf81b11e5a13593157
Beca <234265987+becsnr@users.noreply.github.com>
2026-07-03 10:07:16 -0300

    form

---
+++ js/form.js
@@ -0,0 +1,15 @@
+emailjs.init("zHU9oy_Qog4XQ2n6G");
+
+const form = document.getElementById("contato-form");
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    emailjs.sendForm('service_jz62z4p', 'template_rvf3yy9', this)
+        .then(function() {
+            alert('E-mail enviado com sucesso!');
+            form.reset();
+        }, function(error) {
+            alert('Erro ao enviarr...' + JSON.stringify(error));
+        });
+})
